d216b0d40fea26e1ce0fe00c4e7aac2e2dc377d9
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-04-10 23:12:57 +0200

    Version 1

---
+++ Day One/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <!-- Variable: Memory Location That Stores Data-->
+    <?php 
+    $name = "John Doe";
+    echo $name;
+    ?>
+
+    <!-- Data Types: Scalar Types, Arrays, Objects -->
+
+    <?php
+    // Scalar Types
+    $string = "Daniel";
+    $int = 12345;
+    $float = 123.45;
+    $bool = true;
+    ?>
+
+    <?php
+    // Arrays
+    $array = array("Banana", "Apple", "Orange");
+
+    // Objects
+    // $object = new Car();
+    ?>
+
+    <!-- Example For Day One -->
+    <?php
+       $name = "Sylejman Durguti";
+    ?>
+
+    <p>My name is <?php echo $name; ?>, I'am a frontend dev looking forward to backend</p>
+</body>
+</html>
