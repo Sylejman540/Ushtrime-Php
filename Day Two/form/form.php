50d817179b6e46ff6012c888c4f21bc33d7165fd
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-04-12 16:32:27 +0200

    Version 4

--- Day Two/form/form.php
+++ Day Two/form/form.php
@@ -1,10 +1,13 @@
 <?php
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+/*if($_SERVER["REQUEST_METHOD"] == "POST"){
     $name = $_POST["name"];
     $surname = $_POST["surname"];
     
     echo "<h1>Hello $name  $surname</h1>";
 } else {
     echo "Please submit the form.";
-}
+} */
+
+
+
--- Day Two/index.php
+++ Day Two/index.php
@@ -6,7 +6,7 @@
   <title>Document</title>
 </head>
 <body>
-  <form action="form/form.php" method="post">
+  <!-- <form action="form/form.php" method="post">
     <label for="name">Firstname</label>
     <input type="text" name="name" id="name">
     
@@ -14,6 +14,48 @@
     <input type="text" name="surname" id="surname">
   
     <button type="submit">Submit</button>
+  </form> -->
+
+  <form action="" method="post">
+    <input type="number" name="num1" placeholder="Enter a number">
+    <select name="operator">
+      <option value="add">+</option>
+      <option value="subtract">-</option>
+      <option value="multiply">*</option>
+      <option value="divide">/</option>
+    </select>
+    <input type="text" name="num2" placeholder="Enter a number">
+
+    <button>Calculate</button>
   </form>
+
+  <?php 
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+      $num1 = $_POST["num1"];
+      $num2 = $_POST["num2"];
+      $operator = $_POST["operator"];
+      
+      switch($operator){
+        case "add";
+          $result = $num1 + $num2;
+          break;
+        case "subtract";
+          $result = $num1 - $num2;
+          break;
+        case "multiply";
+          $result = $num1 * $num2;
+          break;
+        case "divide";
+          $result = $num / $num2;
+        break;
+        default:
+          echo "Invalid operator";
+      }
+
+      echo"<h1>The result is $result</h1>";
+
+      
+    }
+  ?>
 </body>
 </html>
